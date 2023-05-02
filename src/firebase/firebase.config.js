2bfb5e2f3137d4a709577018ade7715787bc7ee8
topurayhan007 <topurayhan007@gmail.com>
2023-05-02 14:02:45 +0600

    hosting & GithubAuth App done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvvqFWyfZcfxq6NjfIs06RAHHinDacMsc",
+  authDomain: "chef-recipe-hunter-clien-6e668.firebaseapp.com",
+  projectId: "chef-recipe-hunter-clien-6e668",
+  storageBucket: "chef-recipe-hunter-clien-6e668.appspot.com",
+  messagingSenderId: "75870009731",
+  appId: "1:75870009731:web:d3a85702ae6f6fa4849eb6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
