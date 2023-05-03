9349d4ee0bf912f56a0196184480ec061308fc69
topurayhan007 <topurayhan007@gmail.com>
2023-05-04 04:43:31 +0600

    Footer done

--- src/pages/Homepage/CookingTip/CookingTip.jsx
+++ src/pages/Homepage/CookingTip/CookingTip.jsx
@@ -23,7 +23,7 @@ const CookingTip = () => {
         site.
       </p>
       <div
-        className="hero md:min-h-screen h-[450px]"
+        className="hero lg:h-[550px] h-[450px]"
         style={{
           backgroundImage: `url("/assets/images/tip-bg.jpg")`,
         }}
@@ -31,12 +31,15 @@ const CookingTip = () => {
         <div className="hero-overlay bg-black bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-2xl">
-            <div className="bg-base-300 backdrop-blur-sm bg-white/30 text-white card p-5 h-80 flex justify-center items-center">
+            <div className="bg-base-300 backdrop-blur-sm bg-white/30 text-white card md:p-5 lg:p-10 h-80 flex justify-center items-center">
               <div>
                 <h1 className="mb-5 text-xl md:text-3xl font-bold">
                   {tip.tip}
                 </h1>
-                <p className="mb-5 font-bold text-base md:text-lg text-[#ffc919]">
+                <div className="flex justify-center mb-2">
+                  <hr className="border-[1.2px] w-16 border-orange-400  rounded" />
+                </div>
+                <p className="mb-5 font-extrabold text-base md:text-xl text-green-500">
                   ~ {tip.author}
                 </p>
               </div>
